088551ae555d070903a3a2a19f92da554466a465
Evaldas Buzas <evaldasbuz@gmail.com>
2015-08-15 09:21:32 +0300

    added taking a photo functionality

--- app/src/main/java/evaldasbuz/com/ribbit/MainActivity.java
+++ app/src/main/java/evaldasbuz/com/ribbit/MainActivity.java
@@ -2,24 +2,126 @@ package evaldasbuz.com.ribbit;
 
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
-
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
+
 import com.parse.ParseAnalytics;
 import com.parse.ParseUser;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    public static final int TAKE_PHOTO_REQUEST = 0;
+    public static final int TAKE_VIDEO_REQUEST = 1;
+    public static final int PICK_PHOTO_REQUEST = 2;
+    public static final int PICK_VIDEO_REQUEST = 3;
+
+    public static final int MEDIA_TYPE_IMAGE = 4;
+    public static final int MEDIA_TYPE_VIDEO = 5;
+
+    protected Uri mMediaUri;
+
+
+    protected DialogInterface.OnClickListener mDialogListener =
+            new DialogInterface.OnClickListener() {
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which){
+                case 0: //Take picture
+                    Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    mMediaUri = getOutputMediaUri(MEDIA_TYPE_IMAGE);
+                    if (mMediaUri == null) {
+                        // display an error
+                        Toast.makeText(
+                                MainActivity.this,
+                                R.string.error_external_storage,
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                        startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
+                    }
+                    break;
+                case 1: //Take video
+                    break;
+                case 2: //Choice picture
+                    break;
+                case 3: //Choice video
+                    break;
+            }
+        }
+                private Uri getOutputMediaUri(int mediaType) {
+                    if (isExternalStorageAvailable()){
+                        //get the URI
+
+                        // 1. Get the external storage
+                        String appName = MainActivity.this.getString(R.string.app_name);
+                        File mediaStorageDir = new File(
+                                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                                appName);
+                        // 2. Create our own subdirectory
+                        if (! mediaStorageDir.exists()){
+                           if (mediaStorageDir.mkdirs()){
+                               Log.e(TAG, "Failed to create directory.");
+                               return null;
+                           }
+                        }
+                        // 3. Create a file name
+                        File mediaFile;
+                        Date now = new Date();
+                        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(now);
+
+                        String path = mediaStorageDir.getPath() + File.separator;
+                        if (mediaType == MEDIA_TYPE_IMAGE){
+                            mediaFile = new File(path + "IMG_" + timestamp + ".jpg");
+                        }
+                        else if (mediaType == MEDIA_TYPE_VIDEO){
+                            mediaFile = new File(path + "VID_" + timestamp + ".mp4");
+                        } else {
+                            return null;
+                        }
+                        Log.d(TAG, "File: " + Uri.fromFile(mediaFile));
+                        // 4. Return the file's URI
+                        return Uri.fromFile(mediaFile);
+
+                    } else {
+                        return null;
+                    }
+                }
+
+                private boolean isExternalStorageAvailable(){
+                    String state = Environment.getExternalStorageState();
+                    if (state.equals(Environment.MEDIA_MOUNTED)){
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+    };
+
+
+
 
     SectionsPagerAdapter mSectionsPagerAdapter;
     ViewPager mViewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +151,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            // add it to the Gallery
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            mediaScanIntent.setData(mMediaUri);
+            sendBroadcast(mediaScanIntent);
+        }
+        else if (resultCode != RESULT_CANCELED){
+            Toast.makeText(this, R.string.general_error, Toast.LENGTH_LONG).show();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,13 +176,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
-        if (itemId == R.id.action_logout){
-            ParseUser.logOut();
-            navigateToLogin();
-        } else if (itemId == R.id.action_edit_friends){
-            Intent intent = new Intent(this, EditFriendsActivity.class);
-            startActivity(intent);
+
+        switch (itemId){
+            case R.id.action_logout:
+                ParseUser.logOut();
+                navigateToLogin();
+                break;
+            case R.id.action_edit_friends:
+                Intent intent = new Intent(this, EditFriendsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.action_camera:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setItems(R.array.camera_choices, mDialogListener);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                break;
         }
+
 
         return super.onOptionsItemSelected(item);
     }
